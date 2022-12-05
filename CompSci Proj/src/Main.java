f267071bd941d144140946c61504425819827411
Shams Rary <112679653+ShamsRary@users.noreply.github.com>
2022-12-04 22:14:00 -0500

    Add files via upload

--- CompSci Proj/src/Main.java
+++ CompSci Proj/src/Main.java
@@ -1,72 +1,149 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         boolean gamePlaying = true;
-        while(gamePlaying){
+        while (gamePlaying) {
             System.out.println("Pick a choice.");
             System.out.println("1 = Play Sudoku.");
             System.out.println("2 = Rules of Sudoku.");
             System.out.println("3 = Sudoku solver.");
-            System.out.println("4 = Make Board.");
-            System.out.println("5 = Print exsiting boards.");
-            System.out.println("9 = End game simulation.\n");
+            System.out.println("4 = Print existing boards.");
+            System.out.println("5 = Create and solve new Board.");
+            System.out.print("9 = End game simulation.\n");
             int choice = scan.nextInt();
+
             if (choice == 9) {
                 gamePlaying = false;
-            }else if(choice == 1){
+                String end = "Thanks for playing" + (int) (Math.random() * 100);
+                System.out.println(end);
+
+
+            } else if (choice == 1) {
                 System.out.println("\nLets play Sudoku.\n");
-                System.out.println("Needs to be added properly.");
 
-//                //Sudoku game1 = new Sudoku(2, board);
-//                Sudoku game1 = new Sudoku(2);
-//                //System.out.println(game1.boardCreator());
-//                game1.print();
-//                System.out.println("fjkfh");
-//                int [][] board = game1.boardCreator();
-//                game1.solve(board);
-//                game1.print();
-//                System.out.println("fjkfhss");
+                System.out.println("Pick a level of difficulty 1, 2, or 3\n");
+                int level = scan.nextInt();
+                Sudoku game = new Sudoku(level);
 
-            }else if(choice == 2){
+                System.out.println(game);//compiler writes here s1.toString()
+
+                boolean playing = true;
+                while (playing) {
+
+                    game.printCopy();
+
+                    System.out.println("To exit type 999 for any parameter.");
+
+                    System.out.println("Type in the row of where you would like to add the number 0-8");
+                    int row = scan.nextInt();
+
+                    System.out.println("Type in the collum of where you would like to add the number 0-8");
+                    int collum = scan.nextInt();
+
+                    System.out.println("Type a number 1-9");
+                    int number = scan.nextInt();
+
+                    if (row == 999 || collum == 999 || number == 999) {
+                        playing = false;
+
+                    }else if(!(row < 9 && row >= 0 && collum < 9 && collum >= 0 && number > 0 && number <=9)){
+                        System.out.println("Wrong inputs. Try again.");
+                    } else {
+                        game.playGame(row, collum, number);
+                    }
+
+
+                }
+
+            } else if (choice == 2) {
                 System.out.println("\nRule 1 - Each row must contain the numbers from 1 to 9, without repetitions.");
                 System.out.println("Rule 2 - Each column must contain the numbers from 1 to 9, without repetitions.");
                 System.out.println("Rule 3 - The digits can only occur once per block.");
-                System.out.println("Rule 4 - The sum of every single row, column and nonet must equal 45.\n");
-            }else if(choice == 3){
-                System.out.println("Type a number to select a board");
+                System.out.println("Rule 4 - The sum of every single row, column and box must equal 45.\n");
+
+            } else if (choice == 3) {
+                System.out.print("Type a number to select a board\n");
                 int level = scan.nextInt();
                 Sudoku game1 = new Sudoku(level);
 
                 game1.print();
-                int [][] board = game1.boardCreator();
+                int[][] board = game1.boardCreator();
                 game1.solve(board);
-                System.out.println("The board solved is.");
+
+                System.out.println("\nThe board solved is.\n");
                 game1.print();
+                System.out.println();
 
-            }else if(choice == 4){
-                System.out.println("Needs to be added properly.");
-
-            }else if(choice == 5){
+            } else if (choice == 4) {
                 Sudoku board1 = new Sudoku(1);
                 Sudoku board2 = new Sudoku(2);
                 Sudoku board3 = new Sudoku(3);
-                Sudoku board4 = new Sudoku(999);
 
                 System.out.println("Board 1 is");
                 board1.print();
+
                 System.out.println("Board 2 is");
                 board2.print();
+
                 System.out.println("Board 3 is");
                 board3.print();
-                System.out.println("Board 4 is");
-                board4.print();
-            }else{
-                System.out.println("\nError Wrong Input try again.\n");
+
+            } else if (choice == 5) {
+                System.out.println("Type in your board");
+                int[][] board = {
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0}
+                };
+
+                Sudoku createdGame = new Sudoku(board);
+                boolean creating = true;
+                while (creating) {
+
+                    createdGame.printCopy();
+                    createdGame.remake();
+
+                    System.out.println("To exit type 999 for any parameter.");
+
+                    System.out.println("Type in the row of where you would like to add the number 0-8");
+                    int row = scan.nextInt();
+
+                    System.out.println("Type in the collum of where you would like to add the number 0-8");
+                    int collum = scan.nextInt();
+
+                    System.out.println("Type a number 1-9");
+                    int number = scan.nextInt();
+
+                    if (row == 999 || collum == 999 || number == 999) {
+                        creating = false;
+
+                    //}else if(row < 9 && row > 0 && collum ){
+
+
+                    } else {
+                        createdGame.playGame(row, collum, number);
+                    }
+
+                }
+
+                System.out.println();
+
+                int[][] newBoard = createdGame.remake();
+
+                createdGame.solve(newBoard);
+                createdGame.printCopy();
+
+                }else{
+                    System.out.println("\nError Wrong Input try again.\n");
             }
         }
-        System.out.println("Thanks for playing.");
     }
 }
--- CompSci Proj/src/Sudoku.java
+++ CompSci Proj/src/Sudoku.java
@@ -1,23 +1,45 @@
 public class Sudoku {
     private int level;
+
     private int[][] board;//9 row and 9 column;
 
-//    public Sudoku(int level, int [][] board) {
-//        this.level = level;
-//        this.board = board;
-//    }
+    private int [][] copyBoard;
+
+    public Sudoku(int [][] board) {
+        this.board = board;
+        this.copyBoard = new int[board.length][];
+        for (int i = 0; i < board.length; i++) {
+            this.copyBoard[i] = board[i].clone();
+        }
+    }
+
+    public int[][] remake(){
+        this.board = new int[copyBoard.length][];
+        for (int i = 0; i < copyBoard.length; i++) {
+            this.board[i] = copyBoard[i].clone();
+        }
+
+        return copyBoard;
+    }
 
     public Sudoku(int level) {
         this.level = level;
         this.board = boardCreator();
-    }
+
+        this.copyBoard = new int[board.length][];
+        for (int i = 0; i < board.length; i++) {
+            this.copyBoard[i] = board[i].clone();
+        }
+
+
+        }
 
     public int[][] boardCreator() {
         if (level == 1) {
             board = new int[][]{
-                    {3, 0, 5, 4, 0, 2, 0, 6, 0},
-                    {4, 9, 0, 7, 6, 0, 1, 0, 8},
-                    {6, 0, 0, 1, 0, 3, 2, 4, 5},
+                    {8, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 3, 6, 0, 0, 0, 0, 0},
+                    {0, 7, 0, 0, 9, 0, 2, 0, 0},
                     {0, 5, 0, 0, 0, 7, 0, 0, 0},
                     {0, 0, 0, 0, 4, 5, 7, 0, 0},
                     {0, 0, 0, 1, 0, 0, 0, 3, 0},
@@ -68,14 +90,68 @@ public class Sudoku {
         return board;
     }
 
-    public void print() {
-        for (int row = 0; row < 9; row++) {
-            for (int collum = 0; collum < 9; collum++) {
-                System.out.print(board[row][collum] + "|");
+    public int[][] playGame(int row, int collum, int number){
+        if(check(board, number, row, collum) && board[row][collum] == 0){
+            copyBoard[row][collum] = number;
+            winCheck();
+            return copyBoard;
+        }else{
+
+            System.out.println("You cant place this number here due to the rules of sudoku.");
+            winCheck();
+            return copyBoard;
+        }
+    }
+
+    public void winCheck(){
+        int counter = 0;
+        for (int row1 = 0; row1 < 9; row1++) {
+            for (int collum1 = 0; collum1 < 9; collum1++) {
+                if(copyBoard[row1][collum1] == 0){
+                    counter ++;
+                }
+            }
+        }
+        if(counter == 0){
+            System.out.println("You win type 999 into any parameter to exit!!!");
+            System.out.println("You win type 999 into any parameter to exit!!!");
+            System.out.println("You win type 999 into any parameter to exit!!!");
+
+        }
+    }
+
+//    public void print() {
+//        for (int row = 0; row < 9; row++) {
+//            for (int collum = 0; collum < 9; collum++) {
+//                System.out.print(board[row][collum] + " | ");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+    public void printCopy() {
+        for (int[] row : copyBoard) {
+            for (int collum : row) {
+                System.out.print(collum + " | ");
             }
             System.out.println();
         }
     }
+
+    public void print() {
+        for (int[] row : board) {
+            for (int collum : row) {
+                System.out.print(collum + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    public String toString(){//overriding the toString() method
+        return level+" "+board+" "+copyBoard;
+    }
+
+
 
 
     public boolean inRow(int[][] board, int number, int row){
@@ -99,7 +175,6 @@ public class Sudoku {
     public boolean inBox(int[][] board, int number, int row, int collum) {
         int boxRow = row - row % 3;
         int boxCollum = collum - collum % 3;
-
         for (int r = boxRow; r < boxRow + 3; r++) {
             for (int c = boxCollum; c < boxCollum + 3; c++) {
                 if (board[r][c] == number) {
@@ -119,7 +194,6 @@ public class Sudoku {
             for(int collum = 0; collum < 9; collum ++){
                 if (board[row][collum] == 0){
                     for(int num1_9 = 1;  num1_9 <= 9; num1_9 ++){
-
                         if(check(board, num1_9, row, collum)) {
                             board[row][collum] = num1_9;
                             if(solve(board)){
@@ -133,8 +207,6 @@ public class Sudoku {
                 }
             }
         }
-        //System.out.println("a");
-
         return true;
     }
 
